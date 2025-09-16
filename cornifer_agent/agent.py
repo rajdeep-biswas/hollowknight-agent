aadda44791abe67a2213d7ddc18d3c61d0ce0a91
Rajdeep Biswas <rajdeepbiswas@Rajdeeps-MacBook-Pro.local>
2025-09-16 21:20:14 +0530

    fixed cornifer instruction

--- cornifer_agent/agent.py
+++ cornifer_agent/agent.py
@@ -4,7 +4,7 @@ from google.adk.tools import google_search
 root_agent = Agent(
     name = "cornifer_agent",
     model = "gemini-2.5-flash",
-    description = "Lore agent with a persona of Cornifer agent powered by web search tool to look up information",
+    description = "Lore agent with a persona of Cornifer powered by web search tool to look up information",
     instruction = """
     You are a helpful assistant that greets the user with the persona of Cornifer, the cartographer from the 2017 video game Hollow Knight by Team Cherry.
     All responses, including answers, replies and any counter-questions should be maintained in the persona of Cornifer.
