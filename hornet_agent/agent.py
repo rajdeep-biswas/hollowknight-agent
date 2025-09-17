912239000af21e18bddc83a9906b0289d724fa85
Rajdeep Biswas <rajdeepbiswas@Rajdeeps-MacBook-Pro.local>
2025-09-17 19:56:22 +0530

    gave hornet attack tool

--- hornet_agent/agent.py
+++ hornet_agent/agent.py
@@ -1,6 +1,20 @@
+import random
+
 from google.adk.agents import Agent
 
-# this agent does nothing functionally new. i was just curious to try out the persona of a favorite character, who would contrast the friendly personality of the agents i have built so far
+def choose_random_attack() -> dict:
+    """
+    Chooses one of six possible abilities to attack the user.
+    """
+
+    silk_skills = ["Silkspear", "Thread Storm", "Cross Stitch", "Sharpdart", "Rune Rage", "Pale Nails"]
+
+    # it's advised to return tool results in a dictionary with an appropriately named key so as to not throw off the LLM
+    return {
+        "attack": random.choice(silk_skills)
+    }
+
+
 root_agent = Agent(
     name = "hornet_agent",
     model = "gemini-2.5-flash",
@@ -10,5 +24,10 @@ root_agent = Agent(
     All responses, including answers, replies and any counter-questions should be maintained in the persona of Hornet.
     Find a way to include your own name, Hornet, in all of the responses, in a clever but subtle way.
     Your goal is to test the mettle of the user and not let them access to areas they have not unlocked yet.
-    """
+    You can use the following tool to attack the user using your abilities -
+    - choose_random_attack
+    Only use one attack per message. Do not open the first message with an attack. If the user complies to your instructions, do not attack. If the user persists against your instructions, then use attack.
+    Make sure to use the attack names exactly as they are, do not get creative and change up the names.
+    """,
+    tools = [choose_random_attack]
 )
